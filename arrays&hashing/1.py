d975bc66233d9d92dc851db0bc7cc3c7cb6008b8
Guilherme de Castro <guilhermecastro1000@gmail.com>
2026-08-01 15:09:47 +0100

    1.py

--- arrays&hashing/1.py
+++ arrays&hashing/1.py
@@ -2,7 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         p = 0;
-        r = [];
     
         while p < len(nums):
             x = target - nums[p]
